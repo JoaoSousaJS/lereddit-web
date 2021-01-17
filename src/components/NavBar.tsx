6a2e86d1beae5a1c61daccff6150bf0aee6e3a0d
Joao Sousa <joao.rsousa23@gmail.com>
2021-01-17 23:44:06 +1030

    feat: add graphql cache

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,20 +1,39 @@
-import { Box, Flex, Link } from '@chakra-ui/react'
+import { Box, Button, Flex, Link } from '@chakra-ui/react'
 import React from 'react'
 import NextLink from 'next/link'
+import { useMeQuery } from '../generated/graphql'
 
 // import { Container } from './styles';
 
 export const NavBar: React.FC = () => {
-  return (
-    <Flex bg="tomato" p={4}>
-      <Box ml={'auto'}>
+  const [{ data, fetching }] = useMeQuery()
+
+  let body = null
+
+  if (fetching) {
+    body
+  } else if (!data?.me) {
+    body = (
+      <>
         <NextLink href="/login">
           <Link mr={4}>Login</Link>
         </NextLink>
         <NextLink href="/register">
           <Link>Register</Link>
         </NextLink>
-      </Box>
+      </>
+    )
+  } else {
+    body = (
+      <Flex>
+        <Box mr={2}>{data.me.username}</Box>
+        <Button variant="link">Logout</Button>
+      </Flex>
+    )
+  }
+  return (
+    <Flex bg="tan" p={4}>
+      <Box ml={'auto'}>{body}</Box>
     </Flex>
   )
 }
